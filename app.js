ce1cebe2fa27dd8838dd767bb962eeb4af992bb4
monishwankhade10 <monwan786@gmail.com>
2021-02-14 16:47:25 +0530

    nothing the nazi war

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     
     // Send the response body as "Hello World"
-    response.end('Hello World\n'+re["topic"]);
+    response.end('Hello World\n'+json.stringify(re));
  }).listen(port);
  
  // Console will print the message
