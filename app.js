0138f4126e21ad305ee19b8c23fa9eb37ae28c13
monishwankhade10 <monwan786@gmail.com>
2021-02-14 14:08:36 +0530

    nothing gets married

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+var http=require('http');
+var port=process.env.PORT;
+
 http.createServer(function (request, response) {
     // Send the HTTP header 
     // HTTP Status: 200 : OK
@@ -6,7 +9,7 @@ http.createServer(function (request, response) {
     
     // Send the response body as "Hello World"
     response.end('Hello World\n');
- }).listen(8081);
+ }).listen(port);
  
  // Console will print the message
- console.log('Server running at http://127.0.0.1:8081/');
+ console.log('Server running at '+port);
