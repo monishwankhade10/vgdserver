a48272f5215e7f8724c6bc460758e336e4346dd1
monishwankhade10 <monwan786@gmail.com>
2021-02-14 15:09:54 +0530

    the sons of nothing

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     
     // Send the response body as "Hello World"
-    response.end('Hello World\n'+JSON.stringify(res));
+    response.end('Hello World\n'+res('1'));
  }).listen(port);
  
  // Console will print the message
