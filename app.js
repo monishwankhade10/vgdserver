9b0880f7172b2713b29902e423818074c7358bef
monishwankhade10 <monwan786@gmail.com>
2021-02-14 17:03:14 +0530

    nothing the nazi war

--- app.js
+++ app.js
@@ -2,24 +2,24 @@ var http=require('http');
 var port=process.env.PORT;
 var mysql = require('mysql');
 
-var con = mysql.createConnection({
-    host: "freedb.tech",
-    user: "freedbtech_monishravinandan",
-    password: "Monish@1234"
-  });
-var res;
-  con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-    var sql="select topic from topics where topic_id=45";
-    con.query("use freedbtech_vgddb");
-    con.query(sql, function (err, result) {
-        if (err) throw err;
-        res=result;
-        console.log("Result: " + JSON.stringify(result));
-      });
+// var con = mysql.createConnection({
+//     host: "freedb.tech",
+//     user: "freedbtech_monishravinandan",
+//     password: "Monish@1234"
+//   });
+// var res;
+//   con.connect(function(err) {
+//     if (err) throw err;
+//     console.log("Connected!");
+//     var sql="select topic from topics where topic_id=45";
+//     con.query("use freedbtech_vgddb");
+//     con.query(sql, function (err, result) {
+//         if (err) throw err;
+//         res=result;
+//         console.log("Result: " + JSON.stringify(result));
+//       });
     
-  });
+//   });
 
 http.createServer(function (request, response) {
     // Send the HTTP header 
@@ -28,7 +28,7 @@ http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     
     // Send the response body as "Hello World"
-    response.end('Hello World\n'+json.stringify(re));
+    response.end('Hello World\n');
  }).listen(port);
  
  // Console will print the message
