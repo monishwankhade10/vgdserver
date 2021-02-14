00c1e62c6f249e6b6d65b34ab18d026e1dd19ad6
monishwankhade10 <monwan786@gmail.com>
2021-02-14 14:42:04 +0530

    nothisg strikes back

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 var http=require('http');
 var port=process.env.PORT;
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "freedb.tech",
+    user: "freedbtech_monishravinandan",
+    password: "Monish@1234"
+  });
+
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var sql="select * from user_info";
+    con.query("use freedbtech_vgddb");
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Result: " + JSON.stringify(result));
+      });
+    
+  });
 
 http.createServer(function (request, response) {
     // Send the HTTP header 
@@ -8,7 +27,7 @@ http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     
     // Send the response body as "Hello World"
-    response.end('Hello World\n');
+    response.end('Hello World\n'+JSON.stringify(result));
  }).listen(port);
  
  // Console will print the message
